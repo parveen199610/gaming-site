56bc8fe0bb2dffa6f5cf7b7cb1c5311fec976846
parveen199610 <pk9955196@gmail.com>
2026-02-18 14:16:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+body{
+margin:0;
+font-family:Arial;
+background:#0f0f0f;
+color:white;
+text-align:center;
+}
 
+header{
+padding:20px;
+font-size:28px;
+background:black;
+box-shadow:0 0 20px lime;
+}
+
+.menu{
+margin:20px;
+}
+
+.menu button{
+padding:12px 20px;
+margin:8px;
+background:lime;
+border:none;
+cursor:pointer;
+font-weight:bold;
+border-radius:5px;
+transition:0.3s;
+}
+
+.menu button:hover{
+background:white;
+}
+
+canvas{
+background:black;
+border:2px solid lime;
+margin-top:20px;
+}
+
+.ad-box{
+margin:20px auto;
+padding:20px;
+width:80%;
+background:#1a1a1a;
+border:1px dashed gray;
+}
